5e83ea8c9659e1e6aa2f4d737b80d21a8474599f
Art Ramirez <artramirez08@gmail.com>
2023-02-14 19:04:11 -0800

    added navbar w/ style TODO: fix images and places view/router

--- views/home.jsx
+++ views/home.jsx
@@ -1,3 +1,4 @@
+
 const React = require('react')
 const Def = require('./default')
 
@@ -8,7 +9,7 @@ function home(){
           <h1>HOME</h1>
           <div>
             <img
-              src="public\images\chia-fruit-drink.jpg"
+              src='./public/images/chia-fruit-drink.jpg'
               alt="Chia Fruit Shake"
             />
             <div>
