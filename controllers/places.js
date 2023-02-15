a26e24e2fb7083b2e7bd23cff9c72d08635bc8b4
Art Ramirez <artramirez08@gmail.com>
2023-02-14 19:08:43 -0800

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -28,4 +28,18 @@ router.get('/', (req,res) => {
     }
 )
 
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
+});
+
+
+
+
 module.exports = router
