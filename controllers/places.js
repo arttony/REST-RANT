e5a53ea1b9374e8f48fda3c8a1fa2d4e4d57b2e2
Art Ramirez <artramirez08@gmail.com>
2023-02-25 20:13:49 -0800

    TODO fix put route

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
+//index
+router.get('/', (req,res) => { 
+    
+      res.render('places/index', {places})
+    }
+)
 
 //create
 router.post("/", (req, res) => {
@@ -23,12 +29,6 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/', (req,res) => { 
-    
-      res.render('places/index', {places})
-    }
-)
-
 //show
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
@@ -51,6 +51,32 @@ router.delete("/:id", (req, res) => {
   } else {
     places.splice(id, 1);
     res.redirect("/places");
+  }
+});
+
+//put
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
   }
 });
 
